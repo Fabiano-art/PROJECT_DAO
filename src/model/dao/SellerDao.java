58dce592eb073adbf8860f40769add19b22c5c83
Fabiano-art <fabio.dub@outlook.com>
2020-11-17 19:06:54 -0300

    'SellerDao' criado

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	public void insert (Seller Seller);
+	public void update (Seller Seller);
+	public void deleteById (Integer id);
+	public Seller findById(Integer id);
+	public List<Seller> findAll();
+	
+}
