92ec94047ba13bbc1ded17ea9710358126427777
Fabiano-art <fabio.dub@outlook.com>
2020-11-17 19:08:35 -0300

    Classe de implementação dos Daos criada

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller Seller) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller Seller) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+
+}
