589f377622f5ca270db517f925e062a31ec0f41f
unknown <fabio.dub@outlook.com>
2020-11-18 18:25:19 -0300

    Adicionado método 'findByDepartment'

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -11,5 +12,6 @@ public interface SellerDao {
 	public void deleteById (Integer id);
 	public Seller findById(Integer id);
 	public List<Seller> findAll();
+	public List<Seller> findByDepartment(Department dep);
 	
 }
